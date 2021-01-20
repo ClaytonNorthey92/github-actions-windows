fc7baaa54781eb01fd15ac8433f0d45af74c95aa
ClaytonNorthey92 <clayton.northey@gmail.com>
2021-01-20 06:50:16 -0800

    Showing how not to build Docker Image on Windows

---
+++ dockerfiles/sayhi.Dockerfile
@@ -0,0 +1,2 @@
+# some image that is valid for Windows
+FROM golang:1.14
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("this is my current directory: %s", dir)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	client, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	buildContext, err := archive.TarWithOptions(".", &archive.TarOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	if runtime.GOOS == "windows" {
+		_, err = client.ImageBuild(ctx, buildContext, types.ImageBuildOptions{
+			Context:    buildContext,
+			Dockerfile: "dockerfiles\\sayhi.Dockerfile",
+		})
+	} else {
+		_, err = client.ImageBuild(ctx, buildContext, types.ImageBuildOptions{
+			Context:    buildContext,
+			Dockerfile: "dockerfiles/sayhi.Dockerfile",
+		})
+	}
+
+	if err != nil {
+		panic(err)
+	}
+}
